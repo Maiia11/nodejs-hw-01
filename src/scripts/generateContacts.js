075867b80d51e7a3c41e12cc2c4fa75416117763
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-07-20 18:01:20 +0300

    generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,22 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from "../utils/createFakeContact.js";
 
-const generateContacts = async (number) => {};
+import * as fs from 'node:fs/promises';
+
+const generateContacts = async (number) => {
+    try {
+        const fileContentDB = await fs.readFile(PATH_DB, 'utf8');
+        const existingContacts = JSON.parse(fileContentDB);
+        const newContacts = Array.from({ length: number }, createFakeContact);
+        const updatedContacts = [...existingContacts, ...newContacts];
+        const data = JSON.stringify(updatedContacts, null, 2);
+        await fs.writeFile(PATH_DB, data, 'utf8');
+        console.log(`Successfully wrote ${number} contacts to ${PATH_DB}`);
+    } catch (err) {
+        console.error("Error:", err);
+    }
+
+
+};
 
 generateContacts(5);
