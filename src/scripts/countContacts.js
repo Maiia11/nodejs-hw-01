f6362c776af04e7ee2f7e08a9b9ad19af66ad185
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-07-21 15:01:02 +0300

    add logic getAllContacts and countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,10 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+    const fileContentDB = await fs.readFile(PATH_DB, 'utf8');
+    const allContacts = JSON.parse(fileContentDB);
+    return allContacts.length;
+};
 
 console.log(await countContacts());
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+    const fileContentDB = await fs.readFile(PATH_DB, 'utf8');
+    const allContacts = JSON.parse(fileContentDB);
+    return allContacts;
+
+};
 
 console.log(await getAllContacts());
