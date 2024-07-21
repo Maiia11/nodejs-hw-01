b2523091f80729fb9bb5120166afa862c95b5429
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-07-21 13:20:32 +0300

    addOneContact logic

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,22 @@
-import { PATH_DB } from '../constants/contacts.js';
 
-export const addOneContact = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from "../utils/createFakeContact.js";
+
+import * as fs from 'node:fs/promises';
+
+export const addOneContact = async () => {
+    try {
+        const fileContentDB = await fs.readFile(PATH_DB, 'utf8');
+        const existingContacts = JSON.parse(fileContentDB);
+        const dataOneContact = createFakeContact();
+        existingContacts.push(dataOneContact);
+        const updatedContacts = JSON.stringify(existingContacts, null, 2);
+        await fs.writeFile(PATH_DB, updatedContacts, 'utf8');
+        console.log('Contact added:', dataOneContact);
+    } catch (err) {
+        console.error("Error:", err);
+
+    }
+};
 
 addOneContact();
