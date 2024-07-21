bd9b20230d94bd04bf73112ab55fa12b35829452
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-07-21 15:09:26 +0300

    add logic removeAllContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -2,9 +2,16 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const countContacts = async () => {
+    try {
     const fileContentDB = await fs.readFile(PATH_DB, 'utf8');
     const allContacts = JSON.parse(fileContentDB);
     return allContacts.length;
+
+    } catch (err) {
+        console.error("Error:", err);
+
+    }
+
 };
 
 console.log(await countContacts());
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -2,9 +2,15 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const getAllContacts = async () => {
+    try {
     const fileContentDB = await fs.readFile(PATH_DB, 'utf8');
     const allContacts = JSON.parse(fileContentDB);
     return allContacts;
+
+    } catch (err) {
+        console.error("Error:", err);
+    }
+
 
 };
 
